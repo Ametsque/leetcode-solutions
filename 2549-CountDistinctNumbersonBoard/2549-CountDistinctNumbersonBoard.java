0b96f4379628f9672cb74fbe648498a37e7e84a5
Ametsque <137003275+Ametsque@users.noreply.github.com>
2025-04-29 19:36:01 +0100

    Create file

---
+++ 2549-CountDistinctNumbersonBoard/2549-CountDistinctNumbersonBoard.java
@@ -0,0 +1,10 @@
+// Last updated: 29/04/2025, 19:36:00
+class Solution {
+    public int distinctIntegers(int n) {
+        if (n==1) {
+            return 1;
+        }
+
+        return n-1;
+    }
+}
